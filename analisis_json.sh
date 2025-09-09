2ac84d3e66325d5f3bb156e0e9a06bd141228ce3
Juanfran Tomas <juanfrandeveloper@gmail.com>
2025-09-09 20:26:33 +0200

    Agregar archivo .gitignore, mejorar script de análisis JSON y crear archivo de log

--- analisis_json.sh
+++ analisis_json.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "analisis"
+echo "Análisis de JSON"
+echo "=================="
+echo "Petición de estaciones de la provincia de Valencia" > log.txt
+# curl -sS -H "Accept: application/json" \
+# "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroProvincia/46" > datos/estacionesValencia.json 2>> log.txt
+
+
+url='https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroProvincia/46'
+
+archivoGuardar="datos/estacionesValencia.json"
+
+status=$(curl -sS -H "Accept: application/json" -o "$archivoGuardar" -w '%{http_code}' "$url")
+
+if [ "$status" -ge 200 ] && [ "$status" -lt 300 ]; then
+  echo "OK: $status"
+  echo "Datos guardados en $archivoGuardar $(date)" >> log.txt
+else
+  echo "A fallado la petición de $url. Error HTTP: $status $(date)" >> log.txt
+fi
